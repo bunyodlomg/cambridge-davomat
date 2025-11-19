52c2bb6d4b6e48957d2f49bf1ad29c8b8c684a02
bunyodlomg <bunyodlomg@gmail.com>
2025-11-19 17:20:34 +0500

    a

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,14 +1,26 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import React, { useContext } from "react";
-
+import React, { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
-
 import TelegramLogin from "../pages/TelegramLogin";
 import AdminDashboard from "../pages/AdminDashboard";
 import TeacherDashboard from "../pages/TeacherDashboard";
 
 export default function AppRoutes() {
     const { user } = useContext(AuthContext);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            setAuthToken(token);
+            const userData = JSON.parse(localStorage.getItem("user"));
+            setUser(userData);
+        }
+
+        setLoading(false);
+    }, [user]);
+
+    if (loading) return <div>Loading...</div>; // yoki spinner
 
     return (
         <Routes>
@@ -18,7 +30,7 @@ export default function AppRoutes() {
                 element={
                     !user ? (
                         <Navigate to="/" />
-                    ) : user.role === "admin" ? (
+                    ) : user.role === "admin" && user.isApproved ? (
                         <AdminDashboard />
                     ) : (
                         <TeacherDashboard />
