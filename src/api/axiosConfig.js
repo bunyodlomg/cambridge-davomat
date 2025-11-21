94d08422e0294f5f805a64862e9772a421e63980
bunyodlomg <bunyodlomg@gmail.com>
2025-11-21 11:57:50 +0500

    login err

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-axiosInstance.interceptors.request.use(config => {
-    const token = localStorage.getItem("token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
+// JWT tokenni har bir requestga qo‘shish
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
 
 export default axiosInstance;
