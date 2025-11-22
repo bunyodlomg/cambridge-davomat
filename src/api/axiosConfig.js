384f9509b3a2148b967bff1d25965d31089af20f
bunyodlomg <bunyodlomg@gmail.com>
2025-11-22 20:34:23 +0500

    axios

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-    headers: {
-        "Content-Type": "application/json",
-    },
+    headers: { "Content-Type": "application/json" },
+    withCredentials: true
 });
+
 
 axiosInstance.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
