b5355567f5c62a1b67787df39d875dd3eec4396b
bunyodlomg <bunyodlomg@gmail.com>
2025-11-24 08:23:52 +0500

    tg widget size

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
         script.src = "https://telegram.org/js/telegram-widget.js?22";
         script.async = true;
         script.setAttribute("data-telegram-login", "cambridge_davomat_bot");
-        script.setAttribute("data-size", "small");
+        script.setAttribute("data-size", "medium");
         script.setAttribute("data-request-access", "write");
         script.setAttribute("data-onauth", "window.onTelegramAuthCallback(user)");
 
